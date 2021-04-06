a7ed2dde008c613451ba8a4e320a59898c02329b
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2021-04-06 17:52:07 -0300

    update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,8 @@
-sudo rm -r -v /usr/include/ufjfmltk
-sudo rm -v /usr/lib/x86_64-linux-gnu/libufjfmltk*
-sudo rm -v /usr/lib/x86_64-linux-gnu/pkgconfig/ufjfmltk*
+prefix=/usr/local
+if [ ! -z $1 ]; then
+    prefix=$1
+fi
+
+sudo rm -r -v $prefix/include/ufjfmltk
+sudo rm -v $prefix/lib/x86_64-linux-gnu/libufjfmltk*
+sudo rm -v $prefix/lib/x86_64-linux-gnu/pkgconfig/ufjfmltk*
