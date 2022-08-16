c763c4e48348e76a931c89345b380df990828f89
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2022-08-16 15:04:06 -0300

    Added plotting for individual points

--- main.cpp
+++ main.cpp
@@ -3,23 +3,15 @@
 using namespace mltk::classifier;
 
 int main(){
-    mltk::Data data = mltk::datasets::make_blobs(100, 2, 2, 1, -10, 10, true, 42).dataset;
+    mltk::Data data = mltk::datasets::make_blobs(100, 3, 3, 1, -10, 10, true, 42).dataset;
 
     std::cout << "size: " << data.size() << std::endl;
     std::cout << "dim: " << data.dim() << std::endl;
     std::cout << "classes distribution: " << mltk::Point<size_t>(data.classesDistribution()) << std::endl;
 
-    BalancedPerceptron ima(data);
-    // ima.setMaxTime(20000);
+    mltk::visualize::Visualization<double> plotter;
 
-    bool converged = ima.train();
-
-    std::cout << "converged: " << ((converged)?"Reached convergency":"Did not reached convergency") << std::endl;
-    if(!converged) return 0;
-    mltk::visualize::Visualization vis(data);
-    auto solution = ima.getSolution();
-    std::cout << solution.w << " " << solution.bias << " " << solution.norm  << " " << solution.margin << " " << solution.margin/solution.w[1] << std::endl;
-    vis.plot2DwithHyperplane(0, 1, ima.getSolution());
+    plotter.plot3D(data.getFeature(0), data.getFeature(1), data.getFeature(2));
 
     return 1;
 }
